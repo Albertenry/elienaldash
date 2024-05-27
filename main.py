83f5652f92e66b139778abcf1aff14ad3f240d06
Albertenry <albertenry2@gmail.com>
2024-05-27 14:28:04 -0300

    ajustando requirements

--- main.py
+++ main.py
@@ -12,9 +12,12 @@ import Paginas.pagina_suporte as pagina_suporte
 import Paginas.ordem_de_servico as ordem_de_servico
 
 # Função para carregar o CSS
+
+
 def load_css(file_name):
     with open(file_name) as f:
         st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
 
 # Carregar o CSS personalizado
 load_css("style.css")
@@ -46,11 +49,14 @@ pagina_mapeamento = {
 }
 
 # Função para renderizar a página selecionada
+
+
 def render_pagina(pagina_selecionada):
     if pagina_selecionada in pagina_mapeamento:
         pagina_mapeamento[pagina_selecionada]()
     else:
         st.error("Página não encontrada!")
+
 
 # Inicialização da página
 render_pagina(selected)

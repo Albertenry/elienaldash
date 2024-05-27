bed55b1ca65a1cf0540bccff3dcd300f91ce26f0
Albertenry <albertenry2@gmail.com>
2024-05-27 14:36:40 -0300

    melhorias no grafico

--- Paginas/pagina_dashboards.py
+++ Paginas/pagina_dashboards.py
@@ -4,7 +4,6 @@ import pandas as pd
 import plotly.express as px
 import numpy as np
 
-
 def conectar_bd():
     conn = psycopg2.connect(
         host="localhost",
@@ -14,7 +13,6 @@ def conectar_bd():
     )
     return conn
 
-
 def obter_dados_clientes():
     conn = conectar_bd()
     cur = conn.cursor()
@@ -22,7 +20,6 @@ def obter_dados_clientes():
     clientes = [row[0] for row in cur.fetchall()]
     conn.close()
     return clientes
-
 
 def obter_dados_equipamentos():
     conn = conectar_bd()
@@ -32,7 +29,6 @@ def obter_dados_equipamentos():
     conn.close()
     return equipamentos
 
-
 def obter_dados_meses():
     conn = conectar_bd()
     cur = conn.cursor()
@@ -40,7 +36,6 @@ def obter_dados_meses():
     meses = [row[0] for row in cur.fetchall()]
     conn.close()
     return meses
-
 
 def obter_dados_filtrados(cliente, equipamento, meses):
     conn = conectar_bd()
@@ -55,16 +50,13 @@ def obter_dados_filtrados(cliente, equipamento, meses):
     conn.close()
     return dados
 
-
 def calcular_metricas(dados):
-    df = pd.DataFrame(
-        dados, columns=['mes', 'disponibilidade', 'indisponibilidade', 'mttr', 'mtbf'])
+    df = pd.DataFrame(dados, columns=['mes', 'disponibilidade', 'indisponibilidade', 'mttr', 'mtbf'])
     disponibilidade_media = df['disponibilidade'].mean()
     indisponibilidade_media = df['indisponibilidade'].mean()
     mttr_media = df['mttr'].mean()
     mtbf_media = df['mtbf'].mean()
-    return disponibilidade_media, indisponibilidade_media, mttr_media, mtbf_media, df
-
+    return df, disponibilidade_media, indisponibilidade_media, mttr_media, mtbf_media
 
 def render_dashboard():
     st.title("Dashboard")
@@ -74,16 +66,13 @@ def render_dashboard():
     meses = obter_dados_meses()
 
     cliente_selecionado = st.selectbox("Selecione o Cliente", clientes)
-    equipamento_selecionado = st.selectbox(
-        "Selecione o Equipamento", equipamentos)
+    equipamento_selecionado = st.selectbox("Selecione o Equipamento", equipamentos)
     meses_selecionados = st.multiselect("Selecione o(s) Mês(es)", meses)
 
     if cliente_selecionado and equipamento_selecionado and meses_selecionados:
-        dados = obter_dados_filtrados(
-            cliente_selecionado, equipamento_selecionado, meses_selecionados)
+        dados = obter_dados_filtrados(cliente_selecionado, equipamento_selecionado, meses_selecionados)
         if dados:
-            disponibilidade, indisponibilidade, mttr, mtbf, df = calcular_metricas(
-                dados)
+            df, disponibilidade, indisponibilidade, mttr, mtbf = calcular_metricas(dados)
 
             # Cabeçalho com as métricas
             dst1, dst2, dst3, dst4 = st.columns([1, 1, 1, 1])
@@ -104,27 +93,15 @@ def render_dashboard():
                 st.write('**MTBF:**')
                 st.info(f"{mtbf:.2f} horas")
 
+            # Gráfico de disponibilidade
             st.markdown('---')
-            st.markdown('')
             st.markdown('---')
-
-            # Adicionar o gráfico de evolução da disponibilidade
-            df['mes'] = pd.Categorical(
-                df['mes'], categories=meses, ordered=True)
-            df = df.sort_values('mes')
-            fig = px.line(df, x='mes', y='disponibilidade',
-                          title='Evolução da Disponibilidade por Mês')
-
-            # Exibir o gráfico em duas colunas
-            col1, col2 = st.columns([1, 1])
-            with col1:
-                st.plotly_chart(fig, use_container_width=True)
-
+            fig = px.line(df, x='mes', y='disponibilidade', title='Evolução da Disponibilidade')
+            st.plotly_chart(fig, use_container_width=True)
         else:
             st.warning("Nenhum dado encontrado para os filtros selecionados.")
     else:
         st.info("Por favor, selecione o cliente, equipamento e os meses.")
-
 
 # Chama a função para renderizar o dashboard
 render_dashboard()
